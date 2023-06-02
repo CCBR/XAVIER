572925faa2e3e1e7097840cccd225f1793a5f58a
slsevilla <slsevilla@gmail.com>
2023-06-02 11:29:22 -0400

    allow for output dir to be given at runtime, mark new pipeline runmodes

--- run_exome_pipeline.sh
+++ run_exome_pipeline.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
 runmode=$1
+output_dir=$2
+
+# sh run_exome_pipeline.sh init /data/sevillas2/WES_hg38
 
 module load snakemake/7.19.1
 module load singularity/3.10.5
@@ -9,21 +12,21 @@ if [[ $runmode == "init" ]]; then
     ./exome-seek run \
     --runmode init \
     --input .tests/*.R?.fastq.gz \
-    --output /data/sevillas2/WES_hg38 \
+    --output $output_dir \
     --genome hg38 \
     --targets .tests/Agilent_SSv7_allExons_hg38.bed
 elif [[ $runmode == "dryrun" ]]; then
     ./exome-seek run \
     --runmode dryrun \
     --input .tests/*.R?.fastq.gz \
-    --output /data/sevillas2/WES_hg38 \
+    --output $output_dir \
     --genome hg38 \
     --targets .tests/Agilent_SSv7_allExons_hg38.bed
 elif [[ $runmode == "run" ]]; then
     ./exome-seek run \
     --runmode run \
     --input .tests/*.R?.fastq.gz \
-    --output /data/sevillas2/WES_hg38 \
+    --output $output_dir \
     --genome hg38 \
     --targets .tests/Agilent_SSv7_allExons_hg38.bed
 else
