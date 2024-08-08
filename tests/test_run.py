82058f0200beae9960ac1bc83aa28705c8d402eb
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-08 13:52:20 -0400

    test: dryrun on biowulf

--- tests/test_run.py
+++ tests/test_run.py
@@ -12,7 +12,7 @@ def test_dryrun():
     if get_hpcname() == "biowulf":
         with tempfile.TemporaryDirectory() as tmp_dir:
             run_args = argparse.Namespace(
-                runmode="dryrun",
+                runmode="init",
                 input=list(glob.glob(xavier_base(".tests/*.fastq.gz"))),
                 output=tmp_dir,
                 genome="hg38",
@@ -31,33 +31,12 @@ def test_dryrun():
                 tmp_dir=get_tmp_dir(None, tmp_dir),
                 threads=2,
             )
-
-            # execute dry run and capture stdout/stderr
-            allout = run_in_context(run_args)
-        assert (
-            "This was a dry-run (flag -n). The order of jobs does not reflect the order of execution."
-            in allout
-        )
-
-
-run_args = argparse.Namespace(
-    runmode="dryrun",
-    input=list(glob.glob(xavier_base(".tests/*.fastq.gz"))),
-    output="tmp",
-    genome="hg38",
-    targets=xavier_base(".tests/Agilent_SSv7_allExons_hg38.bed"),
-    mode="local",
-    job_name="pl:xavier",
-    callers=["mutect2", "mutect", "strelka", "vardict", "varscan"],
-    pairs=xavier_base(".tests/pairs.tsv"),
-    ffpe=False,
-    cnv=False,
-    wait=False,
-    create_nidap_folder=False,
-    silent=False,
-    singularity_cache=os.environ.get("SINGULARITY_CACHEDIR", None),
-    sif_cache=get_sif_cache_dir(),
-    tmp_dir=get_tmp_dir(None, "tmp"),
-    threads=2,
-)
-run(run_args)
+            # init
+            allout_1 = run_in_context(run_args)
+            run_args.runmode = "dryrun"
+            # dryrun
+            allout_2 = run_in_context(run_args)
+        assert (all([
+            "--Initializing" in allout_1,
+            "This was a dry-run (flag -n). The order of jobs does not reflect the order of execution." in allout_2
+            ]))
